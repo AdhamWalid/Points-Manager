2bfc53d185b39f97e36b169f05ea573a3dfc788f
AdhamWalid <Adhamwalid682@gmail.com>
2022-04-17 15:27:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on("messageCreate", async (message) => {
 
 
 client.on('messageCreate', async (message) => {
-if (message.content.startsWith ('d!points')){
+if (message.content === 'd!points'){
    
   const target = message.mentions.users.first() || message.author; // Grab the target.
 
